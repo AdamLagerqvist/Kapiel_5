0d75a862d45b7b50f09b30734519c78afc26b25b
AdamLagerqvist <adam.lagerqvist@elev.ga.ntig.se>
2020-10-27 20:53:39 +0100

    5.1 5.2

--- src/Kap5/Uppgift_5_2.java
+++ src/Kap5/Uppgift_5_2.java
@@ -1,16 +1,15 @@
 package Kap5;
 
-import javax.swing.*;
+import java.util.Scanner;
 
 public class Uppgift_5_2 {
     public static void main(String[] args) {
         double d = 1;
-        int n = Integer.parseInt(JOptionPane.showInputDialog("n?"));
-        int k = 1;
+        Scanner tgb = new Scanner(System.in);
+        int n = Integer.parseInt(tgb.nextLine());
         double summa = 0;
-        while (k <= n){
+        for (int k = 1; k <= n; ++k){
             summa = d / k + summa;
-            ++k;
         }
         System.out.print("summan blir " + summa);
     }
